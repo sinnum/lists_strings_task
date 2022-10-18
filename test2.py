e8db01312098c7cbf47a7efa8b24e525884a1cac
dymsson <dymsson@yandex.ru>
2022-10-18 08:54:39 +0700

    Create test2.py

---
+++ test2.py
@@ -0,0 +1,5 @@
+import change_string;
+
+def test2():
+    assert change_string.change_string('Abracadabra', 1, 2, 'Salam') == ('acarmalaSA', 10)
+    assert change_string.change_string('Hello World', 4, 7, 'Beautiful') == ('ufituaeBlleH', 12)
